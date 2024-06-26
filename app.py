1b0edcdad6c9e27ae597b6e79e60859881a75415
rayaanoidPrime <rayaan4dev@gmail.com>
2024-06-26 15:45:53 +0530

    minor

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import joblib
 from model import CryptoAddressFeatureExtractor  
 
@@ -12,10 +12,13 @@ label_encoder = joblib.load('label_encoder.joblib')
 def health():
     return "Hello, World!"
 
-@app.route('/predict/<address>', methods=['GET'])
-def predict_blockchain(address):
+@app.route('/predict/', methods=['GET'])
+def predict_blockchain():
     try:
-
+        address = request.args.get('address')
+        if not address:
+            raise ValueError("Missing crypto_address parameter")
+        
         prediction = model.predict([address])[0]
         predicted_blockchain = label_encoder.inverse_transform([prediction])[0]
     
@@ -23,11 +26,15 @@ def predict_blockchain(address):
             'address': address,
             'predicted_blockchain': predicted_blockchain
         }), 200
+    except ValueError as ve:
+        return jsonify({
+            'error': str(ve)
+        }), 400
     except Exception as e:
         return jsonify({
-            'error': str(e),
-            'address': address,
-        }), 400
+            'error': str(e)
+        }), 500
+   
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
